efda87b47a8f6374e9b95a620a38af2a44f822f4
furkan <frknlty4@gmail.com>
2024-10-21 13:49:22 +0200

    The first dynamic page

---
+++ src/main/java/nl/miwnn/se14/furkan/footballclubdemo/FootballClubDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.furkan.footballclubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballClubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballClubDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se14/furkan/footballclubdemo/FootballClubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.furkan.footballclubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballClubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
